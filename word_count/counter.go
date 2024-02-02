93c57710a6453017652227ad1087397531c57d18
TTraveller7 <yinghuo777@gmail.com>
2024-02-02 13:40:15 +0800

    update

--- word_count/counter.go
+++ word_count/counter.go
@@ -8,20 +8,28 @@ import (
 	"github.com/TTraveller7/invokerlib"
 )
 
-var counterPc = &invokerlib.ProcessorCallbacks{
-	OnInit:  counterInit,
-	Process: counterProcess,
-}
+var (
+	counterPc = &invokerlib.ProcessorCallbacks{
+		OnInit:  counterInit,
+		Process: counterProcess,
+	}
+	stateStore invokerlib.StateStore
+)
 
 func CounterHandler(w http.ResponseWriter, r *http.Request) {
 	invokerlib.ProcessorHandle(w, r, counterPc)
 }
 
 func counterInit() {
-	fmt.Printf("counterInit called")
+	var err error
+	stateStore, err = invokerlib.NewFreeCacheStateStore()
+	if err != nil {
+		fmt.Printf("create state store failed: %v\n", err)
+		return
+	}
+	invokerlib.AddStateStore("counter_state_store", stateStore)
 }
 
 func counterProcess(ctx context.Context, record *invokerlib.Record) error {
-	fmt.Printf("%s\n", string(record.Value))
-	return nil
+	return stateStore.Put(ctx, record.Key, record.Value)
 }
