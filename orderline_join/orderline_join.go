6608dd127bc8d42bcc4cbf6c1edcde49009444a5
TTraveller7 <yinghuo777@gmail.com>
2024-04-01 02:55:01 +0800

    update

--- orderline_join/orderline_join.go
+++ orderline_join/orderline_join.go
@@ -8,6 +8,7 @@ import (
 	"github.com/TTraveller7/invokerlib/pkg/logs"
 	"github.com/TTraveller7/invokerlib/pkg/models"
 	"github.com/TTraveller7/invokerlib/pkg/utils"
+	"github.com/bytedance/sonic"
 )
 
 var (
@@ -21,6 +22,30 @@ func OrderlineJoinHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func orderlineJoin(ctx context.Context, leftRecord *models.Record, rightRecord *models.Record) error {
-	logs.Printf("orderline invoked: leftRecord: %s, rightRecord: %s", utils.SafeJsonIndent(leftRecord), utils.SafeJsonIndent(rightRecord))
+	var order Order
+	if err := sonic.Unmarshal(leftRecord.Value(), &order); err != nil {
+		return err
+	}
+	var orderline Orderline
+	if err := sonic.Unmarshal(rightRecord.Value(), &orderline); err != nil {
+		return err
+	}
+	if order.Oid != orderline.Oid {
+		return nil
+	}
+	fullOrderline := &FullOrderline{
+		Wid:       order.Wid,
+		Did:       order.Did,
+		Oid:       order.Oid,
+		CarrierId: order.CarrierId,
+		OlNumber:  orderline.OlNumber,
+		Iid:       orderline.Iid,
+		DeliveryD: orderline.DeliveryD,
+		Amount:    orderline.Amount,
+		SupplyWid: orderline.SupplyWid,
+		Quantity:  orderline.Quantity,
+		DistInfo:  orderline.DistInfo,
+	}
+	logs.Printf("full order line produced: %s", utils.SafeJsonIndent(fullOrderline))
 	return nil
 }
